6f7d1fde8ca2d43fe38285b9c086aa9cc31ec430
Lucas EECKHOUTTE <lucas1.eeckhoutte@epitech.eu>
2026-04-14 22:53:53 +0200

    [+] Add configuration management and server functionality
    - Introduced a configuration file (config.toml) for video and webtoon settings.
    - Updated go.mod to include the BurntSushi/toml package for TOML parsing.
    - Implemented LoadConf and ChangeVFolder functions in server/server.go for configuration handling.
    - Enhanced main.go to support user commands for changing configuration and displaying current settings.

--- server/server.go
+++ server/server.go
@@ -1,7 +1,36 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"log"
 
-func ServerStart() {
+	"github.com/BurntSushi/toml"
+)
+
+type conf struct {
+	VideoF   string `toml:"video_f"`
+	WebtoonF string `toml:"webtoon_f"`
+}
+
+func LoadConf(path string) (conf, error) {
+	var cfg conf
+	_, err := toml.DecodeFile(path, &cfg)
+	return cfg, err
+}
+
+func ChangeVFolder(cfg *conf) {
+	var ChangePath string
+	fmt.Println("Quelle est le nouveau path ?")
+	fmt.Scan(&ChangePath)
+	cfg.VideoF = ChangePath
+}
+
+func ServerStart() conf {
+	cfg, err := LoadConf("config/config.toml")
+	if err != nil {
+		log.Fatalf("error toml")
+	}
+	fmt.Printf("Debug: %+v\n", cfg)
 	fmt.Println("Serveur Pret")
+	return cfg
 }
