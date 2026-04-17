0cca22088d7e1dad42f932bb60b4955b91532b47
Lucas EECKHOUTTE <lucas1.eeckhoutte@epitech.eu>
2026-04-17 22:17:39 +0200

    [+] Add HttpRequest function and update button for request test

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
+function HttpRequest() {
+  const localhost = "test"
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <section>
+      <button onClick={() => setCount((prev) => prev + 1)}>
+        this is a test ({count})
+      </button>
+      <button onClick={HttpRequest()} class="flex justify-center">
+        request test
+      </button>
       <div class="min-h-screen flex items-center justify-center text-4xl">
         Hello Word
       </div>
