4cfb1c0c0a56cfed2b1d9fe2624973a4c0a1718e
Lucas EECKHOUTTE <lucas1.eeckhoutte@epitech.eu>
2026-04-18 17:11:44 +0200

    [~] Update command handling to remove hyphens and fix SimpleHost call
    [+] Add command cd and ls

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"server/server"
 )
 
@@ -12,30 +13,46 @@ func main() {
 	fmt.Println("Terminal du serveur :")
 	for {
 		var rep string
+		var path string
 		fmt.Print("$MIRA-Stream> ")
 		_, err := fmt.Scan(&rep)
 		if err != nil {
 			log.Fatalln("error input")
 		}
-		if rep == "-exit" {
+		if rep == "exit" {
 			break
 		}
-		if rep == "-vfolder" {
+		if rep == "vfolder" {
 			server.ChangeVFolder(&cfg)
 			fmt.Printf("After change : %+v\n", cfg)
 			continue
 		}
-		if rep == "-list" {
+		if rep == "list" {
 			server.TakeFolder(&cfg)
 			continue
 		}
-		if rep == "-config" {
+		if rep == "config" {
 			server.ViewConfig(cfg)
 			continue
 		}
-		if rep == "-start" && cfg.StartStatut == 0 {
-			go server.SimpleHost()
+		if rep == "start" && cfg.StartStatut == 0 {
+			go server.SimpleHost(cfg)
 			cfg.StartStatut = 1
+			continue
+		}
+		if rep == "cd" {
+			_, err := fmt.Scan(&path)
+			if err != nil {
+				log.Fatalln("error cd")
+			}
+			os.Chdir(path)
+			cfg.Path, err = os.Getwd()
+			fmt.Printf("DEBUG CD : %v\n", cfg.Path)
+			continue
+		}
+		if rep == "ls" {
+			folder, _ := os.ReadDir(cfg.Path)
+			fmt.Printf("DEBUG LS : %+v\n", folder)
 			continue
 		}
 		fmt.Println("Command not found.")
