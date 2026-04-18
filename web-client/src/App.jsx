08e2469d3e4fa0d498c48cc8965703898af73019
Lucas EECKHOUTTE <lucas1.eeckhoutte@epitech.eu>
2026-04-18 17:11:06 +0200

    [+] Add request http with backend

--- web-client/src/App.jsx
+++ web-client/src/App.jsx
@@ -2,7 +2,17 @@ import { useState } from 'react'
 import './App.css'
 
 function HttpRequest() {
-  const localhost = "test"
+  fetch("http://localhost:8000/")
+    .then((res) => {
+      if (!res.ok) throw new Error(`HTTP ${res.status}`)
+      return res.json()
+    })
+    .then((data) => {
+      console.log("JSON OK:", data)
+    })
+    .catch((err) => {
+      console.error("Erreur requête:", err)
+    })
 }
 
 function App() {
@@ -13,10 +23,10 @@ function App() {
       <button onClick={() => setCount((prev) => prev + 1)}>
         this is a test ({count})
       </button>
-      <button onClick={HttpRequest()} class="flex justify-center">
+      <button onClick={HttpRequest} className="flex justify-center">
         request test
       </button>
-      <div class="min-h-screen flex items-center justify-center text-4xl">
+      <div className="min-h-screen flex items-center justify-center text-4xl">
         Hello Word
       </div>
     </section>
