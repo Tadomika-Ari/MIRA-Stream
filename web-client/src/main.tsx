5e32d7749ae7c6a1e1b1d87b816134f0e2dcca49
Lucas EECKHOUTTE <lucas1.eeckhoutte@epitech.eu>
2026-05-06 15:21:04 +0200

    [+] Add main and connection components for video streaming with .tsx extention

---
+++ web-client/src/main.tsx
@@ -0,0 +1,18 @@
+// @ts-nocheck
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+import App from './App.tsx'
+import Conn from './pages/connection.tsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/conn" element={<Conn />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
